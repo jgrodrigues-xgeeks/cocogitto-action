2f83425bbc80f17c8a82f1448a25a034824dc490
João Gabriel Rodrigues <j.g.rodrigues@kigroup.de>
2022-02-22 12:26:02 +0000

    feat: Add install dir property

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-CUR_DIR=$(pwd)
 VERSION=4.1.0
 TAR="cocogitto-$VERSION-x86_64-unknown-linux-musl.tar.gz"
-BIN_DIR="$HOME/.local/bin"
+BIN_DIR="$INSTALL_DIR"
 
 mkdir -p "$BIN_DIR"
-cd "$BIN_DIR" || exit
+pushd "$BIN_DIR" || exit
 curl -OL https://github.com/cocogitto/cocogitto/releases/download/"$VERSION"/"$TAR"
 tar xfz $TAR
-cd "$CUR_DIR" || exit
+popd || exit
